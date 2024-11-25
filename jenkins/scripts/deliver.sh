a823acd46afb3ebb5d568ca78de39e76ac9b7d29
Luca Ilchen <luca.ilchen@ibm.com>
2024-11-25 15:14:30 +0100

    tried copying the target file

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -27,9 +27,9 @@ ls -la
 echo "Target has the following files"
 ls -la target/
 
-xattr -c /target/${NAME}-${VERSION}.jar
+cp target/${NAME}-${VERSION}.jar .
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar /target/${NAME}-${VERSION}.jar
+java -jar ${NAME}-${VERSION}.jar
